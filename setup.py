e710fac1089825d8d5d1733957809c6b8661f4d2
mrHolaDays <semuongluh@gmail.com>
2025-08-24 20:53:55 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="marlib",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[],
+    description="MAR Database Library",
+    keywords="database mar",
+)
